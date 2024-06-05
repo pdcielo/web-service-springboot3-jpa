2f70804818fda4608782c0c7dc0a6f4f164c58ce
Pedro Beltrão (Cielo) <breck_pb@hotmail.com>
2024-06-05 13:13:27 -0300

    Project WebService, User.java - Created User class, UserResource.java - Created UserResource class and method findAll.

---
+++ src/main/java/com/webservice/project/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.webservice.project.resources;
+
+import com.webservice.project.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    //TODO Colocar mascara para email e telefone, e sugestões de complemento para o email.
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1L, "Cristian", "cristian.jonatas@outlook.com", "(75)99938-2278", "918273");
+        return ResponseEntity.ok().body(user);
+    }
+}
